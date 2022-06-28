e73e8c54b087a842632bebb7fc663f69f00d9326
benpinon4 <benjamin.pinon@CodeImmersives.com>
2022-06-28 14:19:32 -0400

    Final Submit

---
+++ main.js
@@ -0,0 +1,40 @@
+let paragraph = document.querySelector("p");
+
+paragraph.style.color = "lightblue";
+
+let header1 = document.querySelector("h1");
+
+header1.style.fontSize = "48px";
+
+paragraph.innerText =
+  "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.";
+
+let item13 = document.querySelector("#item-13");
+
+item13.style.opacity = .3;
+
+let item3 = document.querySelector("#item-3");
+
+item3.innerText = 'I say, "Hi!"';
+
+let image1 = document.querySelector("img");
+
+image1.src =
+  "https://www.rd.com/wp-content/uploads/2020/04/GettyImages-694542042-e1586274805503.jpg";
+
+image1.style.height = "300px";
+
+let image2 = document.querySelector('#image2');
+
+image2.style.height = '300px'
+
+let item16 = document.querySelector('#item-16');
+
+item16.className = 'item'
+
+item16.innerText = "Won't get fooled again."
+
+
+
+
+
